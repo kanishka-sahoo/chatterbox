42e3de150d93ed71d36adb0814fe44f9fb06e558
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2023-02-18 20:54:41 +0530

    Formatting

--- chatbot_tui.py
+++ chatbot_tui.py
@@ -1,8 +1,10 @@
 import openai_interface as oif
 
+
 def main():
     opening_prompt = input("Enter opening prompt: ")
-    conversation = opening_prompt + '''\n\nUser:Hello, how are you?\nAI:'''  # variable to keep track of the conversation
+    # variable to keep track of the conversation.
+    conversation = opening_prompt + '''\n\nUser:Hello, how are you?\nAI:'''
     conversation += oif.get_response(conversation)
     print(conversation)
     user_input = input("--> ")
@@ -12,6 +14,7 @@ def main():
         conversation += resp
         print(resp)
         user_input = input("--> ")
+
 
 if __name__ == "__main__":
     main()
--- openai_interface.py
+++ openai_interface.py
@@ -1,16 +1,19 @@
 '''
-OpenAI Chatbot, Enables a user to communicate with the GPT3 Chatbot in a conversational manner.
+OpenAI Chatbot, Enables a user to communicate with the GPT3 Chatbot in a conver
+sational manner.
 This is the interface between the chatbot and the program.
 Author: Kanishka Sahoo
 '''
-import openai, os
-import time
+import openai
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Set up the OpenAI API key, loaded from a .env file containing the token as API_TOKEN=""
+# Set up the OpenAI API key, loaded from a .env file containing the token as AP
+# I_TOKEN=""
 openai.api_key = str(os.environ["API_TOKEN"])
+
 
 def get_response(conversation):
     # Send the conversation to the OpenAI API
@@ -19,15 +22,16 @@ def get_response(conversation):
         engine="text-davinci-003",
         prompt=(f"{conversation}"),
         max_tokens=2048,
-        n = 1,
+        n=1,
         stop=None,
         temperature=0.7,
     )
     resp = f"{response['choices'][0]['text']}\n"
     return resp
 
+
 def process_user_input(conversation: str, user_input: str):
-    # Checks the user input and allows for sending of commands starting with '/'
+    # Checks the user input and allows for sending of command starting with '/'
     # if not user_input.startswith('/'):
     return "", get_response(conversation)
     '''
